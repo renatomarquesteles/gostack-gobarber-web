2ff0e7437dce3c11d3b56623343f3bb01e8463d9
Renato <renatomarquesteles@gmail.com>
2019-10-12 03:38:32 -0300

    feature: estilos globais

--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: linear-gradient(-90deg, #5f2c82, #49a09d);
+  background: linear-gradient(-90deg, #b7c5d1, #eef2f3);
 `;
--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: #333;
+  background: linear-gradient(-90deg, #b7c5d1, #eef2f3);
 `;
