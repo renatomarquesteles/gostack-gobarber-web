798ff47b9408475cce21c89994e82e3638edab30
Renato <renatomarquesteles@gmail.com>
2019-12-19 10:34:14 -0300

    feature: adiciona logout da aplicaçao

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
+import { signOut } from '~/store/modules/auth/actions';
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import AvatarInput from './AvatarInput';
@@ -14,6 +15,10 @@ export default function Profile() {
 
   function handleSubmit(data) {
     dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
   }
 
   return (
@@ -38,7 +43,9 @@ export default function Profile() {
 
         <button type="submit">ATUALIZAR PERFIL</button>
       </Form>
-      <button type="button">SAIR DO GOBARBER</button>
+      <button type="button" onClick={handleSignOut}>
+        SAIR DO GOBARBER
+      </button>
     </Container>
   );
 }
